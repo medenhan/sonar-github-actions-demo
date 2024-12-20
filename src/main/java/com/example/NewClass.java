52971d9fc029830ef4fb5d119243051cbde65939
Your Name <your_email@example.com>
2024-12-20 13:45:49 -0600

    Added NewClass.java and some intentional code smells

---
+++ src/main/java/com/example/NewClass.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class NewClass {
+
+    public static void doSomething() {
+        int y = 20; // Unused variable
+        System.out.println("This is a new class!");
+    }
+}
